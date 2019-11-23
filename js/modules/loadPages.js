9a979578895f39e118e6b037ed7ff3f84bdf3933
Rayne <tamaramichaud@hotmail.com>
2019-11-23 11:57:46 +0000

    dd: function to load header and footer dynamically; complete

--- js/modules/loadPages.js
+++ js/modules/loadPages.js
@@ -1,66 +1,29 @@
-//$(document).ready(function() {
-//
-////function fetchTopTail(){
-//
-//    
-//    console.log("err... what's happening!!");
-//    
-//    fetch("./includes/header.html").then(response => {
-//        console.log("HEADER CONTENTS: " + response.text());
-////        return response.text()
-//    }).then(data => {
-//        console.log("OR MAYBE: " + data.text());
-////        document.querySelector("header").innerHTML = response;
-//    });
-////
-////    fetch("./includes/footer.html").then(response => {
-////        return response.text()
-////    }).then(data => {
-////        document.querySelector("footer").innerHTML = data;
-////    });
-////}
-//    })
-//
-//
-//
-//
+function loadCommonElements(){
+	
+	const includesArray = [ 'header', 'footer'];
+	$.each(includesArray, function(item) {
+		
+		console.log("item: " + includesArray[item])
+		var pathToFile = "./includes/" + includesArray[item] + ".html";
+		var fileType = "html";
+		var elementId = includesArray[item];
+		
+		readFileContents(pathToFile, fileType, elementId);
+	});
+}
 
 
-
-
-function fetchTopTail(){
-              $.ajax({
-                type: "GET",
-                url: "./includes/header.html",
-                dataType: "html",
-
-
-
-                error: function (e) {
-                    alert("OOPS failed to load json config file!");
-                    console.log("JSON file-reading Failed: ", e);
-                },
-
-
-
-                success: function (headerObj) {
-//console.log("Read the contents well!! ...now what...");
-
-//                    console.log(response.permissionsTypes[1]);
-                    console.log("HEADER FOUND: ")
-//                   $.each(responseObj, function(key, val) {
-//                       
-//                       console.log("key: " + key + ", value: " + val);
-//                       
-                    
-                    
-                    console.log(headerObj);
-                    document.getElementById('header').innerHTML = headerObj;
-                    
-                       
-                   }
-                    
-                
-          })
-    
+function readFileContents(pathToFile, fileType, elementId) {
+	
+	 $.ajax({
+				type: "GET",
+				url: pathToFile,
+				dataType: fileType,
+				error: function (e) {
+						console.log("Failed to read file: " + pathToFile, e);
+				},
+				success: function (fileContents) {
+						document.getElementById(elementId).innerHTML = fileContents;
+				}
+	 }); 
 }
