d8d21a253b79da995ff7007221bba0b379bcc029
U-EMAILVISION\Tamara.Michaud <Tamara.Michaud@UKL-IA0GRSQ.Emailvision.com>
2019-11-27 22:32:22 +0000

    continue building workouts page

--- js/modules/workout.js
+++ js/modules/workout.js
@@ -6,8 +6,9 @@ var workoutPageIds = {
                 },
                 addedit: {
                     wrapper: 'addedit',
-                    dropdown: 'addedit-exercises',
-                    
+                    dropdown: 'addedit-select',
+                    inputs: 'addedit-inputs',
+                    variations: 'addedit-variations'
                 }
 }
 
@@ -20,8 +21,28 @@ document.getElementById(workoutPageIds.main.addNew).addEventListener('click', fu
     });
 
 document.getElementById(workoutPageIds.main.editEx).addEventListener('click', function(){
+        console.log("calling the edit function!");
         showAddEdit('edit');
 });
+
+
+
+var Exercise = function(name, variations, muscleGroups, types){
+    this.name = name;
+    this.variations = variations;
+    this.muscleGroups = muscleGroups;
+    this.types = types;    
+}
+Exercise.prototype.loadAddEdit = function(){
+    
+    var nameElement = document.querySelector(`#${workoutPageIds.addedit.inputs} #name`);
+    nameElement.innerHTML = "exercise name goes here";   
+    nameElement.setAttribute('placeholder', "exercise name goes here");   
+    
+}
+
+
+
 
 
 // 
@@ -66,50 +87,34 @@ function loadWorkouts(allExercisesConfigs){
 
 function showAddEdit(option) {
     
-    const inputWrapperElement = document.getElementById(workoutPageIds.addedit.wrapper);
-/*       "name" : "template",
-            "muscle-groups" : [ ],
-            "workout-type" : [ ],
-            "variations" : [ 
-                    {
-                        "level" : "easy",
-                        "description" : "",
-                        "reps" : 10,
-                        "sets" : 3 
-                    },
-                    {
-                        "level" : "medium",
-                        "description" : "",
-                        "reps" : 15,
-                        "sets" : 5 
-                    },
-                    {
-                        "level" : "hard",
-                        "description" : "",
-                        "reps" : 20,
-                        "sets" : 8 
-                    }*/
-    
-    
-    
-    
-    
-    
-    
-    
+    toggleVisible(workoutPageIds.addedit.wrapper, 'block');
+    //TODO: toggle off the new-workout
+
+    console.log("option: " + option);
     if(option === 'edit'){
-        
+        console.log("toggling...");
     // if edit, display select name dropdown only,
+        
+        toggleVisible(workoutPageIds.addedit.dropdown, 'block');
+        toggleVisible(workoutPageIds.addedit.inputs, 'none');
+    } else {
+        var exercise = new Exercise("exampleExercise", ["easy"], ["brain"], ["strength"] );
+        exercise.loadAddEdit();
+        showInputs();
     }
     //      on select, call showInputs(exercise);
     // if new, showInputs(blank);
+    
     
 }
 
 function showInputs(contents){
     // show addEdit div
-    
+    toggleVisible(workoutPageIds.addedit.dropdown, 'none');
+    toggleVisible(workoutPageIds.addedit.wrapper, 'block');
+    toggleVisible(workoutPageIds.addedit.inputs, 'block');
 }
+
 
 function loadExercisesDropdown(exercisesArray, dropdownElement){
     
@@ -120,16 +125,17 @@ function loadExercisesDropdown(exercisesArray, dropdownElement){
         selectElement.appendChild(optionElement);
     }
     
-    var selectElement =  document.getElementById(dropdownElement);
+    var selectElement =  document.querySelector(`#${dropdownElement}`);
     createOption('select an exercise');
     
     exercisesArray.forEach(nextExercise => {
         createOption(nextExercise.name);
     })
-        
-    console.log("complete");
 }
 
+function toggleVisible(elementId, showhide){
+    document.getElementById(elementId).setAttribute('style', `display:${showhide}`);
+} //toggleVisible(workoutPageIds.addedit.wrapper, 'block'); // none
 
 /* addExercise(){
     get main obj.exerciseTemplate;
