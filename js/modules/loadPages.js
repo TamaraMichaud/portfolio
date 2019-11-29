bb43878e2b078ce05fc6cd41d4b913b4a7c2a49a
Rayne <tamaramichaud@hotmail.com>
2019-11-29 23:52:52 +0000

    refactor: load includes/ using relative referencing

--- js/modules/loadPages.js
+++ js/modules/loadPages.js
@@ -1,58 +1,41 @@
-function loadCommonElements(){
-	
-	const includesArray = [ 'header', 'footer'];
-	$.each(includesArray, function(item) {
-		
-		var pathToFile = "../includes/" + includesArray[item] + ".html";
-		var fileType = "html";
-		var elementId = includesArray[item];
-		
-		readFileContents(pathToFile, fileType, elementId);
-	});
+const relativeDir = getRelativeDir();
+const includesArray = [ 'header', 'footer'];
+const urlPathStart = window.location.protocol + 
+		"//" + window.location.host + "/";
+const projectDir = document.URL
+  		.replace(urlPathStart, "").replace(/\/.*$/, "\/");
+const pathRoot = urlPathStart + projectDir;
+
+
+for(var i = 0; i < includesArray.length; i++){
+	doAjax(includesArray[i]);
 }
 
-//
-//function getServerPath(){
-//    //HACK: new security on my work laptop requires me to use this workaround when developing locally here.
-//    var urlPathStart = window.location.protocol + "//" + window.location.host + "/";
-//    if (window.location.host === "") {
-//        // testing by loading file in browser instead of localhost
-//        return document.URL.replace(urlPathStart, "").replace(/\/[^\/]*$/, '\/');
-//    } else {
-//        
-//	    return document.URL.replace(urlPathStart, "").replace(/\/.*$/, "");
-//    }
-//    //FUCKSAKKKKKEEEE
-//}
+function doAjax(item){
+  $.ajax({
+			type: "GET",
+			url: relativeDir + 'includes/' + item + '.html',
+			dataType: 'html',
+			error: function (e) {
+					console.log("Failed to read file: " + 'x', e);
+			},
+			success: function (fileContents) {
 
-
-function readFileContents(pathToFile, fileType, elementId) {
-	
-//var serverPath = '../includes/'
-//    console.log("Path root: " + "DIEEEEE");
-    var urlPathStart = window.location.protocol + "//" + window.location.host + "/";
-    var projectDir = document.URL.replace(urlPathStart, "").replace(/\/.*$/, "");
-    var pathRoot = urlPathStart + projectDir;
-	$.ajax({
-				type: "GET",
-				url: pathRoot + pathToFile,
-				dataType: fileType,
-				error: function (e) {
-						console.log("Failed to read file: " + pathToFile, e);
-				},
-				success: function (fileContents) {
-
-						document.getElementById(elementId).innerHTML = setHrefLinks(fileContents, pathRoot);
-                    //TODO: we can't do relative references... but thanks to this update, now we can't do full paths either. ARGHHHHHHH
-				}
-	 }); 
+					document.getElementById(item).innerHTML = setHrefLinks(fileContents);
+			}
+	}); 
 }
 
-
-
-function setHrefLinks(fileContents, urlPath){
-
-	return fileContents.replace("href=\"", "href=\"" + urlPath + "/");
+function getRelativeDir(){
+	const folderDepth = document.URL.replace(/^.*portfolio\//, '').replace(/[^\/]*/g, '').length;
+	var relativeDir = '';
+	for(var d = 0; d < folderDepth; d++) {
+		relativeDir += '../';
+	}
+	return relativeDir;
 }
 
-loadCommonElements();
+function setHrefLinks(fileContents){
+
+	return fileContents.replace("href=\"", "href=\"" + pathRoot + "/");
+}
