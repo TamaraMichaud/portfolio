001e03273c582f2477e7d6e3bbb8dd8141f8f74d
Rayne <tamaramichaud@hotmail.com>
2019-12-04 18:17:34 +0000

    refactor: convert time-line creation to class/methods from functions

--- js/modules/work.js
+++ js/modules/work.js
@@ -1,7 +1,8 @@
 import {fetchFileContents} from './FetchConfig.js';
 import {ProjectRecord, JobRecord, EducationRecord} from './work/CareerInfo.js';
+import {TimeLine} from './work/TimeLine.js'
 
-var orbWidth = 7; //TODO: find a way to tie this dynamically to the css
+var orbWidth = 15; //TODO: find a way to tie this dynamically to the css
 var innerWidth = 100;
 var globalConfig;
 fetchFileContents("career").then((jsonContents) => {
@@ -9,12 +10,8 @@ fetchFileContents("career").then((jsonContents) => {
 	uiController.init();
 });
 
-
-//body.addEventListener("resize", positionOrbs);
-
 var uiController = (function(){
 	
-	var timeLine = document.getElementById('timeline');
 	var careerItemArray = [];
 	
 	function buildItemArray(itemList, ClassObject) {
@@ -28,98 +25,19 @@ var uiController = (function(){
 	}
 
 	return {
-	  init: function(){
+		init: function(){
 			console.log('Starting Up');
-		  
-		  // create an array of elements & their "value"
+
+			// create an array of elements & their "value"
 			buildItemArray(globalConfig.jobHistory, JobRecord);
 			buildItemArray(globalConfig.projects, ProjectRecord);
 			buildItemArray(globalConfig.education, EducationRecord);
-		  
-console.log(careerItemArray);
-		  
-		  // get a list of timestamp values
-			
-			var orderedRefsArray = [];
-		   var maxDate = getMaxDate();
-			var minDate = maxDate;
-			// we grab all the dates and print a div per 0^10
-			careerItemArray.forEach(a => {
-				var refValue = (Array.isArray(a.ref)) ? a.ref[0] : a.ref;
-				minDate = (refValue > minDate) ? minDate : refValue;
-				orderedRefsArray.push(refValue);
-			});
-			orderedRefsArray.push(maxDate);
-console.log(orderedRefsArray.sort((a,b)=>a-b));
-			var tlH = timeLine.getBoundingClientRect().top - 49;
-		  	var tlW = (timeLine.clientWidth - orbWidth)  / (orderedRefsArray.length - 1);
-		  
-		   var leftPos = 0; 
-		   var innerYRef = 0;
-		   var innerXRef = 0;
-			orderedRefsArray.forEach((ref, index, fullArray) =>  {
-console.log("REF: " + ref);
-				var item = careerItemArray.find(obj => {
-var arrayitemref = (Array.isArray(obj.ref)) ? obj.ref[0] : obj.ref;
-				console.log("matches " + arrayitemref + "?");
-														  
-														 return arrayitemref === ref});
-console.log("ref:");
-console.log(item);
-				//create orb,
-				var newOrb = document.createElement("div");
-				newOrb.id = "orb-" + index;
-				newOrb.classList.add("orb");
 
-				// set item element invisible
-				var elementObj;
-				if(item) {
-					elementObj = item.element;
-				} else {
-					elementObj = document.createElement("div");
-					elementObj.textContent = "TODAYS DATE";
-				}
-				elementObj.id = "info-" + index;
-				elementObj.style.setProperty("display", "none");
-				elementObj.classList.add(`pos-${innerYRef}`);
-				// append item element
-	
-				newOrb.appendChild(elementObj);
+			var timeline = new TimeLine(careerItemArray, orbWidth);
+			timeline.populateTimeline();
 
-				// bind click action (show item element)
-				newOrb.addEventListener('click', () => {
-
-					var infoEl = document.getElementById(elementObj.id);
-					var currVal = infoEl.style.display;
-					var newVal = (currVal === "none") ? "inline" : "none";
-
-					infoEl.style.setProperty("display", newVal);
-				});
-
-				// append orb at position
-				newOrb.style.setProperty("position", "absolute");
-				newOrb.style.setProperty("top", `${tlH}px`);
-				newOrb.style.setProperty("left", `${leftPos}px`);
-				
-				timeLine.appendChild(newOrb);
-				
-				leftPos += tlW;
-				innerYRef = (innerYRef === 2) ? 0 : ++innerYRef;
-				innerXRef -= innerWidth  / (fullArray.length - 1);
-				
-				var innerElem = document.getElementById(elementObj.id);
-				innerElem.style.left = `${innerXRef}px`;
-
-			});
-	  
-	  }
+			window.addEventListener("resize", () => timeline.reset());
+		}
 	}
         
 })();
-
-
-
-function getMaxDate(){
-	var d = new Date();
-	return Date.UTC(d.getFullYear(), d.getMonth(), d.getDate());
-}
