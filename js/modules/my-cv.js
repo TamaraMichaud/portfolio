16c7e5c8b0be2935ae6bfd834aee075c80c9b19f
U-EMAILVISION\Tamara.Michaud <Tamara.Michaud@UKL-IA0GRSQ.Emailvision.com>
2019-12-10 17:48:42 +0000

    add: start to build my-cv details.

---
+++ js/modules/my-cv.js
@@ -0,0 +1,17 @@
+
+// set default image for linkedInProfile if broken
+
+
+// sod the grid; have the 3 images in a bootstrap carousel
+    // nope; will need to be v.2; carousel of 3 circular images that rotate visibly on x axis
+//(2 fading in the background, one major in the fore, on click/swipe, swivel the three and bring another forward)
+
+
+//v1:
+
+//on-click "MyCv" in header;
+// show full-height, locked-to-rhs, UL/LI menu with 3 thumbnails stacked
+// on-click;
+    // cv: display modal with embed pdf
+    // github, go to page
+    // linked-in, go to page
