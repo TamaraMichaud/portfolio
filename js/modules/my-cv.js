ef183fd2fb1866817f48d1c2a5d0ce6d79a36790
Rayne <tamaramichaud@hotmail.com>
2019-12-10 22:15:00 +0000

    issues with deleting adhoc dom elements...

--- js/modules/my-cv.js
+++ js/modules/my-cv.js
@@ -49,10 +49,11 @@ function toggleSidebar() {
 function destroyModal(){
 	var myCvModal = document.querySelector('.cv-pdf');
 	var parent = document.querySelector('.mycvmodal');
-	console.log(myCvModal);
+//	console.log(myCvModal);
 	console.log(parent);
 //	console.log(parent.firstChild);
-	while(parent.firstChild){
-		parent.removeChild(myCvModal);
-	}
+//	while(parent.firstChild){
+//		parent.removeChild();
+//	}
+	myCvModal.remove();
 }
