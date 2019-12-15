c009c7abaa78d4d4c23079ad788997365a2a41df
Rayne <tamaramichaud@hotmail.com>
2019-12-15 22:32:07 +0000

    refactor: projects added to carousel. css being updated [wip]

--- js/modules/my-cv.js
+++ js/modules/my-cv.js
@@ -15,39 +15,36 @@ var uiController = (function(){
 
 			globalConfig.jobHistory.forEach((obj, idx, array) => {
 				
-				document.querySelector(".job-history").appendChild(new JobRecord(obj));
+				var nextJobRecord = new JobRecord(obj);
+				nextJobRecord.classList.add("col", "col-4", "col-lg-4", "col-md-12", "col-sm-12");
+				document.querySelector(".job-history").appendChild(nextJobRecord);
 			})
 
 			globalConfig.education.forEach((obj, idx, array) => {
 				var selector = ".education";
-//				if(obj.certificate) {
-//					selector = ".certs";
-//				}
+
 				document.querySelector(selector).appendChild(new EducationRecord(obj));
 			})
 
 			
-			
-//			eduItems.forEach((obj, idx, array) => {
-//				
-//				document.querySelector(".education").appendChild(obj.element);
-//			})
-console.log("ok now what?");
-//console.log(careerItemArray);
-//console.log(careerItemArray[2].element);
-//			careerItemArray.forEach((obj, idx, array) =>{
-//				
-//				switch (obj.ref) {
-//					case "job" :			
-//						document.querySelector(".job-history").appendChild(obj.element);
-//						break;
-//					case "education" :
-//						document.querySelector(".education").appendChild(obj.element);
-//						break;
-//				}
-//			})
-//			
-			
+			globalConfig.projects.forEach((obj, idx, array) => {
+
+				var nextProject = new ProjectRecord(obj);
+				nextProject.classList.add('carousel-item');				
+				
+				var indicatorElement = document.createElement('li');
+				indicatorElement.setAttribute('data-target', '#carouselExampleIndicators');
+				indicatorElement.setAttribute('data-slide-to', idx);
+
+				if(idx === 0){
+					nextProject.classList.add("active");
+					indicatorElement.classList.add("active");
+				}
+				document.querySelector(".carousel-inner").appendChild(nextProject);
+				
+				document.querySelector(".carousel-indicators").appendChild(indicatorElement);
+				
+			});
 		
 		}
 	}
